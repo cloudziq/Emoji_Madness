5acb30a4c33bc6366a8d95008ff9f1f6ef733e62
cloudziq <arwu84@gmail.com>
2021-08-31 20:01:37 +0200

    even more bug fixes

--- main/GO/bonuses/level.go
+++ main/GO/bonuses/level.go
@@ -24,7 +24,7 @@ embedded_components {
   position {
     x: 0.0
     y: 0.0
-    z: 0.0
+    z: 1.0
   }
   rotation {
     x: 0.0
@@ -70,6 +70,26 @@ embedded_components {
     x: 0.0
     y: 0.0
     z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "halo"
+  type: "sprite"
+  data: "tile_set: \"/assets/graphic/lights.atlas\"\n"
+  "default_animation: \"emoji_light_big\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.98
   }
   rotation {
     x: 0.0
